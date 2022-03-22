f318a4b50222451b217324cad06e794e8e52ee7d
MiSobecki <michal2000sob@gmail.com>
2022-03-22 16:49:14 +0100

    First challenge: - fix source code by implementing generate method in ReceiptGenerator; - correct boolean operator in TenPercentDiscount; - create empty list in Receipt constructor.

--- src/main/java/com/virtuslab/internship/receipt/ReceiptGenerator.java
+++ src/main/java/com/virtuslab/internship/receipt/ReceiptGenerator.java
@@ -1,14 +1,30 @@
 package com.virtuslab.internship.receipt;
 
 import com.virtuslab.internship.basket.Basket;
+import com.virtuslab.internship.product.Product;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ReceiptGenerator {
 
     public Receipt generate(Basket basket) {
-        List<ReceiptEntry> receiptEntries = new ArrayList<>();
+        List<ReceiptEntry> receiptEntries;
+
+        List<Product> products = basket.getProducts();
+
+        Map<Product, Integer> productsWithQty = new HashMap<>();
+
+        products.forEach(product -> productsWithQty.merge(product, 1, Integer::sum));
+
+        receiptEntries = productsWithQty.entrySet()
+                .stream()
+                .map(product -> new ReceiptEntry(product.getKey(), product.getValue()))
+                .collect(Collectors.toList());
+
         return new Receipt(receiptEntries);
     }
 }
